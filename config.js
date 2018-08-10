b8315b234adb8d9232dbb9fda1069ba14ce63e2a
yu <wymyxlqm@163.com>
2018-08-10 11:52:14 +0800

    加上try catch处理先暂时解决出错挂掉的问题

--- config.js
+++ config.js
@@ -4,11 +4,9 @@ var config = {
     // 当前的url
     forwardUrl: "https://beta.rishiqing.com",
     // 要转发到新版的url
-    // var forwardUrlNew = "https://betanew.rishiqing.com";
     forwardUrlNew: "https://betanew.rishiqing.com",
     // 要转发到新版的url
     forwardUrlOld: "https://betaold.rishiqing.com",
-    // var forwardUrlNew = "https://betanew.rishiqing.com";
     forwardUrlNewReg: new RegExp("^http[s]?:\\/\\/betanew\\.rishiqing\\.com"),
     // 要转发到新版的url
     forwardUrlOldReg: new RegExp("^http[s]?:\\/\\/betaold\\.rishiqing\\.com"),
