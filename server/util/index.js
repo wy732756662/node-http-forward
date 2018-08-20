734394d89463e03c20b2b92e77f37c551e31c928
yu <wymyxlqm@163.com>
2018-08-20 17:28:48 +0800

    兼容新旧版第三方登录注册

--- server/util/index.js
+++ server/util/index.js
@@ -18,12 +18,20 @@ function processRedirectLocation(location,isNew){
 
 // 通过请求的headers来判断当前请求是应该去新版还是旧版
 function isOld(req) {
-  if(req.url.indexOf("/task/v2/register") !== -1){
+  if(req.url.indexOf("/task/v2/register") !== -1
+  ){
     return true;
   }
   if(req.url.indexOf("/task/v1/register")!=-1 ||
       req.url.indexOf("/task/v1/team/invite/verifyCode")!=-1 ||
-      req.url.indexOf("/task/login/success") !== -1){
+      req.url.indexOf("/task/login/success") !== -1 ||
+      req.url.indexOf("/task/v1/sina/loginPage") !== -1 ||
+      req.url.indexOf("/task/v1/weixin/loginPage") !== -1 ||
+      req.url.indexOf("/task/v1/qq/loginPage") !== -1 ||
+      req.url.indexOf("/task/sinaOauth/afterLogin") !== -1 ||
+      req.url.indexOf("/task/weixinOauth/afterLogin") !== -1 ||
+      req.url.indexOf("/task/qqOauth/afterLogin") !== -1
+  ){
       return false;
   }
   var headers = req.headers;
